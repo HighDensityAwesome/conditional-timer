786b629c63156f7524571808821d6ef9b3d36522
HighDensityAwesome <high.dense.awesome@gmail.com>
2016-10-02 00:54:02 +1000

    Added adaptability + user input for filename

--- conditional-timer.py
+++ conditional-timer.py
@@ -2,10 +2,12 @@ import os
 import time
 #from subprocess import Popen, PIPE
 
+filename = input('Enter the exact file that is downloading')
 
 while True:
-    if os.path.exists('/home/dan/Downloads/kali-linux-2016.2-amd64.iso'):
+    if os.path.exists('/home/$WHOAMI/Downloads/%s' % filename):
         time.sleep(5)
         os.system('sudo shutdown -P 20')
         break
-    time.sleep(10)
+    time.sleep(5)
+time.sleep(5)
